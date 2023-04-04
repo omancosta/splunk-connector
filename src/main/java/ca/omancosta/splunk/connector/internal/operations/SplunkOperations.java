aac00d94a19e42266ee9888540df873ed6696322
Oman Costa <oman.nadin@gmail.com>
2023-04-04 14:06:14 -0600

    Enhanced search method of SplunkOperations; added getJobs in splunkOperations

--- src/main/java/ca/omancosta/splunk/connector/internal/operations/SplunkOperations.java
+++ src/main/java/ca/omancosta/splunk/connector/internal/operations/SplunkOperations.java
@@ -4,10 +4,14 @@ import static org.mule.runtime.extension.api.annotation.param.MediaType.ANY;
 
 import ca.omancosta.splunk.connector.internal.config.SplunkConfiguration;
 import ca.omancosta.splunk.connector.internal.connection.SplunkConnection;
+import com.splunk.Job;
 import org.mule.runtime.extension.api.annotation.param.MediaType;
 import org.mule.runtime.extension.api.annotation.param.Config;
 import org.mule.runtime.extension.api.annotation.param.Connection;
+import org.mule.runtime.extension.api.annotation.param.Optional;
+import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
 
+import java.util.Map;
 
 /**
  * This class is a container for operations, every public method in this class will be taken as an extension operation.
@@ -15,15 +19,13 @@ import org.mule.runtime.extension.api.annotation.param.Connection;
 public class SplunkOperations {
 
   @MediaType(value = ANY, strict = false)
-  public String search(@Config SplunkConfiguration configuration, @Connection SplunkConnection connection, String query){
-    return connection.getSplunkService().search(query).values().toString();
+  public String search(@Config SplunkConfiguration configuration, @Connection SplunkConnection connection, String query,  @Optional @DisplayName("Arguments") Map<String, Object> args){
+    Job searchJob = args == null ? connection.getSplunkService().search(query) : connection.getSplunkService().search(query,args);
+    return searchJob.values().toString();
   }
 
-  /**
-   * Example of an operation that uses the configuration and a connection instance to perform some action.
-   */
   @MediaType(value = ANY, strict = false)
-  public String retrieveInfo(@Config SplunkConfiguration configuration, @Connection SplunkConnection connection){
-    return "";
+  public String getJobs(@Config SplunkConfiguration configuration, @Connection SplunkConnection connection) {
+    return connection.getSplunkService().getJobs().toString();
   }
 }
